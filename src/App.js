170bcf4295b5e5005fe618586fe48d545bf64344
Nelly R <nretana1089@gmail.com>
2022-06-28 15:28:41 -0600

    description added to the main app component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className='App'>
       <h1>First React App with Git</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit ipsa, dolor atque ex voluptas dolorem deserunt adipisci incidunt doloribus, modi corporis libero voluptate! Possimus porro praesentium illo ex esse illum?</p>
     </div>
   );
 }
