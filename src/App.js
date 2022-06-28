384e34d64776c39098fdf2b0282cc95fb7bc8724
Nelly R <nretana1089@gmail.com>
2022-06-28 16:02:33 -0600

    Button added to the main component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className='App'>
       <h1>First React App with Git</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit ipsa, dolor atque ex voluptas dolorem deserunt adipisci incidunt doloribus, modi corporis libero voluptate! Possimus porro praesentium illo ex esse illum?</p>
+      <button>See More</button>
     </div>
   );
 }
