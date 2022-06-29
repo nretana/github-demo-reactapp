61fc19cfd6adfb184894c7a1dbc87188380b23a2
Nelly R <nretana1089@gmail.com>
2022-06-29 17:22:58 -0600

    Description text corrected in main app component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className='App'>
       <div className='container'>
         <h1>First React App with Git</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit ipsa, dolor atque ex voluptas dolorem deserunt adipisci incidunt doloribus, modi corporis libero voluptate! Possimus porro praesentium illo ex esse illum?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit ipsa, dolor atque</p>
         <button>Explore More</button>
       </div>
       <div></div>
