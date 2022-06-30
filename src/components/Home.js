c0d8c0aef66eb438300f7967c0eb8ea0850c60c5
Nelly R <nretana1089@gmail.com>
2022-06-29 19:06:55 -0600

    description text updated based on style guie v1.1.1 in react app

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = ()=> {
     return(
         <div className='container'>
             <h1 className='fw-bold'>Your Flexible Smart Dashboard</h1>
-            <p>A very sophisticated dashboard that can help all your finance actiuvities very easily, so it can save your time.</p>
+            <p>An easy dashboard to manage your financial activities easily. Save time!</p>
             <button>Explore More</button>
        </div>
     )
