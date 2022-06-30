a067d859fc22c921181c3af697ffe1c29efed906
Nelly R <nretana1089@gmail.com>
2022-06-29 18:38:41 -0600

    Home header updated in react app

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import styles from './Home.module.scss';
 const Home = ()=> {
     return(
         <div className='container'>
-            <h1 className='fw-bold'>Flexible Smart Dashboard</h1>
+            <h1 className='fw-bold'>Your Flexible Smart Dashboard</h1>
             <p>A very sophisticated dashboard that can help all your finance actiuvities very easily, so it can save your time.</p>
             <button>Explore More</button>
        </div>
