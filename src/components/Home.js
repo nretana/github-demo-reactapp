39c3bb50f1bac097947a61cd2ae5a145f0e351e3
Nelly R <nretana1089@gmail.com>
2022-06-30 17:44:09 -0600

    Correcting Home component in react app

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = ()=> {
     return(
         <div className='container'>
             <h1 className='fw-bold'>Your Flexible Smart Dashboard</h1>
-            <p>Take your next step in your finance activities very easily. Save your time!</p>
+            <p>Take your next step with your finance activities very easily. Save your time!</p>
             <button>Explore More</button>
        </div>
     )
