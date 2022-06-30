772f9281c0484068afad6895ace6177b97cae62a
Nelly R <nretana1089@gmail.com>
2022-06-29 18:28:20 -0600

    Home component added to react app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 
+//components
+import Home from './components/Home';
+
 function App() {
   return (
     <div className='App'>
-      <div className='container'>
-        <h1>First React App with Git</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit ipsa, dolor atque</p>
-        <button>Explore More</button>
-      </div>
-      <div></div>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+
+import styles from './Home.module.scss';
+
+const Home = ()=> {
+    return(
+        <div className='container'>
+            <h1 className='fw-bold'>Flexible Smart Dashboard</h1>
+            <p>A very sophisticated dashboard that can help all your finance actiuvities very easily, so it can save your time.</p>
+            <button>Explore More</button>
+       </div>
+    )
+}
+
+export default Home;
