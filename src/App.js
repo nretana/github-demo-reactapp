31c7721f43f65d5bbb2a03d1223fc166603672a6
Nelly R <nretana1089@gmail.com>
2022-07-01 12:17:07 -0600

    Navigation bar added to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 
 //components
+import NavigationBar from './UI/NavigationBar';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className='App'>
+      <NavigationBar />
       <Home />
     </div>
   );
