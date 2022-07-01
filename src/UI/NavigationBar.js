5b9e107687e7bb36912f492e28e7d70472558026
Nelly R <nretana1089@gmail.com>
2022-07-01 12:43:32 -0600

    Styling for Navigation Bar added in react app

--- src/UI/NavigationBar.js
+++ src/UI/NavigationBar.js
@@ -1,8 +1,10 @@
-import {Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+import styles from './NavigationBar.module.scss';
 
 const NavigationBar = () => {
     return(
-        <Navbar fixed="top" variant="dark">
+        <Navbar fixed="top" variant="dark" className={styles['bg-dark']}>
             <Container>
                 <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                 <Nav className="ms-auto">
