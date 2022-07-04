9415d8de5e1cf2661b86019a11444f999b04a5ed
Nelly R <nretana1089@gmail.com>
2022-07-04 13:12:24 -0600

    styles for text updated in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ const Home = ()=> {
     return(
         <div className={`container ${styles['home-container']}`}>
             <div className='row justify-content-center align-content-center h-100'>
-                <h1 className='fw-bold'>Your Flexible Smart Dashboard</h1>
-                <p>Take your next step with your finance activities easily, Save time!</p>
+                <h1 className='fw-bolder'>Your Flexible Smart Dashboard</h1>
+                <p className='fw-bolder'>Take your next step with your finance activities easily, Save time!</p>
                 <button className='w-auto'>Explore More</button>
             </div>
        </div>
